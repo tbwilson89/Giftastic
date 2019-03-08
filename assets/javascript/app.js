2fb071c2e0ae19b474a5e364f30bf17b4625bc23
Trevor Wilson <trevorbwilson89@gmail.com>
2019-03-07 22:05:55 -0600

    check before adding new favorites to stop duplicates. Cleaning up code more.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,69 +5,68 @@ var favArr = []
 
 function makeBtns(){
   $('#btn-list').empty()
-  gifArr.forEach(creature=>$('#btn-list').append(`<button class='btn btn-primary show-gifs' data-name=${creature}>${creature}</button>`))
-  console.log('test')
+  gifArr.forEach(gif=>$('#btn-list').append(`<button class='btn btn-primary show-gifs' data-name=${gif}>${gif}</button>`))
   $('.show-gifs').on('click', onClickAddGifs)
 }
 
 //function to add gif cards/tiles to on click of buttons after they are created
 function onClickAddGifs(){
-  console.log(this.dataset.name)
   var queryURL = `https://api.giphy.com/v1/gifs/search?q=${this.dataset.name}&api_key=${apiKey}_KEY&limit=10`
   $.ajax({url: queryURL, method: 'GET'}).then((res)=>{
     $('#gif-display').empty();
+    console.log(res.data)
     res.data.forEach((gif)=>{
-      console.log(gif)
+      // console.log(gif)
       addGifTile(gif.images.fixed_height_still.url, gif.images.fixed_height.url, gif.rating, gif.title)
     })
     $('#gif-display').append(`<div class='fill-space'></div>`)
-    $('img').on('click', function(){
-      var animateData = this.dataset.animate
-      var stillData = this.dataset.still
-      var animated = this.dataset.animated
-      console.log(animated)
-      if(animated === "true"){
-        $(this).attr('src', stillData)
-        $(this).attr('data-animated', "false")
-      } else {
-        $(this).attr('src', animateData)
-        $(this).attr('data-animated', "true")
-      }
-    })
+    $('img').on('click', onClickImg)
     $('.add-fav').on('click', function(){
-      console.log($(this).parent().siblings('.card-img'))
       var imgSibling = $(this).parent().siblings('.card-img')
-      var newFav = {
-        still: imgSibling.data('still'),
-        animate: imgSibling.data('animate'),
-        rating: imgSibling.data('rating'),
-        title: imgSibling.data('title')
+      var newFav = imgSibling.data('storage')
+      if(favArr.indexOf(newFav) === -1){
+        favArr.push(newFav)
       }
-      console.log(newFav)
-      favArr.push(newFav)
     })
   })
 }
 
 // function to append HTML for gif card/tile
 function addGifTile(still, animate, rating, title, isFav=false){
+  // console.log(title)
   $('#gif-display').append(`
     <div class="card" style="width: 18rem;">
       <img class="card-img"
-        src=${still}
-        data-animated='false'
-        data-animate=${animate}
-        data-still=${still}
-        data-rating=${rating}
-        data-title=${title}
+        src=${still},
+        data-animated=false,
+        data-storage='{
+          "animate": "${animate}",
+          "still": "${still}",
+          "rating": "${rating}",
+          "title": "${title}"
+        }'
         alt="Card image cap">
       <div class="card-body text-center">
-        <p class="card-text">Rating: ${rating}</p>
+        <p class="card-text">Rating: ${rating.toUpperCase()}</p>
         <h5 class="card-title">${title}</h5>
         ${isFav ? '' : `<a href="#" class="btn btn-primary btn-center add-fav">Add Favorite</a>`}
       </div>
     </div>
   `)
+}
+function onClickImg(){
+  console.log(JSON.parse(this.dataset.storage))
+  var animateData = JSON.parse(this.dataset.storage).animate
+  var stillData = JSON.parse(this.dataset.storage).still
+  var animated = this.dataset.animated
+  console.log(animated)
+  if(animated === "true"){
+    $(this).attr('src', stillData)
+    $(this).attr('data-animated', "false")
+  } else {
+    $(this).attr('src', animateData)
+    $(this).attr('data-animated', "true")
+  }
 }
 
 $('#add-gif-btn').on('click', ()=>{
@@ -78,9 +77,10 @@ $('#add-gif-btn').on('click', ()=>{
 })
 $('#show-favorites').on('click', function(){
   $('#gif-display').empty()
-  favArr.forEach((fav)=>{
-    addGifTile(fav.still, fav.animate, fav.rating, fav.title, true)
-  })
+  console.log(favArr)
+  favArr.forEach((fav)=>{addGifTile(fav.still, fav.animate, fav.rating, fav.title, true)})
+  // ('#gif-display').append(`<div class='fill-space'></div>`)
+  $('img').on('click', onClickImg)
 })
 
 makeBtns()
